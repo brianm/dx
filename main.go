48888fb388d20c2b3072041cacebd7e28bdca4e6
Brian McCallister <brianm@skife.org>
2014-03-01 11:12:48 -0800

    muckery

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Brian McCallister
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
 package main
 
 import (
@@ -7,22 +23,26 @@ import (
 
 func main() {
 	m := martini.Classic()
-
 	m.Use(render.Renderer())
 
-	t := thing{
-		Name: "Brian",
-		Age:  39,
-	}
-
 	m.Get("/", func(r render.Render) {
-		r.JSON(200, t)
+		r.JSON(200, person{
+			Name: "World",
+			Age:  6,
+		})
+	})
+
+	m.Get("/:name", func(p martini.Params, r render.Render) {
+		r.JSON(200, person{
+			Name: p["name"],
+			Age:  39,
+		})
 	})
 
 	m.Run()
 }
 
-type thing struct {
+type person struct {
 	Name string `json:"name"`
 	Age  int    `json:"age"`
 }
