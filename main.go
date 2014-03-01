bfc5bcba4d5714626ae56e4f563942317fed98f7
Brian McCallister <brianm@skife.org>
2014-02-28 21:37:22 -0800

    some json

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/codegangsta/martini"
+)
 
 func main() {
+	m := martini.Classic()
 
-	fmt.Printf("Hello, world!\n")
+	t := thing{
+		Name: "Brian",
+		Age:  39,
+	}
+
+	m.Get("/", func(w http.ResponseWriter) string {
+		w.Header().Set("Content-Type", "application/json")
+		return must(json.Marshal(t))
+	})
+	m.Run()
+}
+
+func must(data []byte, err error) string {
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+type thing struct {
+	Name string `json:"name"`
+	Age  int `json:"age"`
 }
