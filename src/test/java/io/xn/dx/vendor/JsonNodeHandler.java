7f90642bb079f2d7454110546e5d3af869128246
Brian McCallister <brianm@skife.org>
2014-03-26 20:36:26 -0700

    fail on bad http status codes in json handler

--- src/test/java/io/xn/dx/vendor/JsonNodeHandler.java
+++ src/test/java/io/xn/dx/vendor/JsonNodeHandler.java
@@ -2,6 +2,7 @@ package io.xn.dx.vendor;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 
 import java.io.IOException;
@@ -11,6 +12,12 @@ public class JsonNodeHandler implements ResponseHandler<JsonNode>
     @Override
     public JsonNode handleResponse(final HttpResponse response) throws IOException
     {
+        int status = response.getStatusLine().getStatusCode();
+        if (status >= 300 || status < 200) {
+            throw new HttpResponseException(response.getStatusLine().getStatusCode(),
+                                            response.getStatusLine().getReasonPhrase());
+
+        }
         return Jackson.getReader().readTree(response.getEntity().getContent());
     }
 }
